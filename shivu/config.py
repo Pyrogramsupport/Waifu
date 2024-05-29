6a68f400796264af13dfb57099a1c573897ed84c
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-05-29 08:07:51 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,7 +3,7 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = "6679467894"
-    sudo_users = "6679467894"
+    sudo_users = "6679467894 1060707656"
     GROUP_ID = -1002159833810
     TOKEN = "7182024060:AAEV0GAAQUlw_E-UnNTOxd7K3A6dOpR6muM"
     mongo_url = "mongodb+srv://Siva:Siva@cluster0.9ojz8ds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
