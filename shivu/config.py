1d00180413749e9076859dfbe87f26dadb4eb435
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-05-30 22:39:23 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,15 +2,15 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6679467894"
-    sudo_users = "6679467894 1060707656"
+    OWNER_ID = "7350197183"
+    sudo_users = "7350197183"
     GROUP_ID = -1002159833810
     TOKEN = "7328682504:AAEIy7vJ3jfphSVgDE-tA8P7TxQ8NcIX0z8"
     mongo_url = "mongodb+srv://Siva:siva@secure.ffezokv.mongodb.net/?retryWrites=true&w=majority&appName=Secure"
     PHOTO_URL = ["https://telegra.ph/file/b89f8d3daed456c1c5ddf.jpg", "https://telegra.ph/file/18eb0aa99929796cf62bb.jpg"]
     SUPPORT_CHAT = "secure_chattt"
     UPDATE_CHAT = "secure_updates"
-    BOT_USERNAME = "secure_x_robot"
+    BOT_USERNAME = "animecatcher_x_robot"
     CHARA_CHANNEL_ID = "-1002212512545"
     api_id = 19042248
     api_hash = "a1d443cb79941a89c493f22abf4f84cb"
