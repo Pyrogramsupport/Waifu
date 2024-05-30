bdf7405d985317e35c0b4b7e6e85f9e732f5528e
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-05-30 09:39:35 +0530

    Update start.py

--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -1,7 +1,7 @@
 import random
 from html import escape 
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ParseMode
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler, CommandHandler
 
 from shivu import application, PHOTO_URL, SUPPORT_CHAT, UPDATE_CHAT, BOT_USERNAME, db, GROUP_ID
