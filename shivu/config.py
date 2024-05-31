52077b6a383642dbf692a49c11b424b61792b7b4
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-05-31 08:19:33 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -7,7 +7,7 @@ class Config(object):
     GROUP_ID = -1002159833810
     TOKEN = "7328682504:AAEIy7vJ3jfphSVgDE-tA8P7TxQ8NcIX0z8"
     mongo_url = "mongodb+srv://Hanuman:Siva@bot.7afxmsg.mongodb.net/?retryWrites=true&w=majority&appName=Bot"
-    PHOTO_URL = ["https://telegra.ph/file/b89f8d3daed456c1c5ddf.jpg", "https://telegra.ph/file/18eb0aa99929796cf62bb.jpg"]
+    PHOTO_URL = ["https://telegra.ph/file/b89f8d3daed456c1c5ddf.jpg", "https://telegra.ph/file/18eb0aa99929796cf62bb.jpg", "https://telegra.ph/file/fd1c9426df1fe6c8b1ea5.jpg"]
     SUPPORT_CHAT = "animecatchersupport"
     UPDATE_CHAT = "animecatcherupdates"
     BOT_USERNAME = "animecatcher_x_robot"
