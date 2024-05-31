a5450701b59ed9f3d10c1186b1165232fd6c9a01
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-05-31 06:57:45 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -8,10 +8,10 @@ class Config(object):
     TOKEN = "7328682504:AAEIy7vJ3jfphSVgDE-tA8P7TxQ8NcIX0z8"
     mongo_url = "mongodb+srv://Siva:siva@secure.ffezokv.mongodb.net/?retryWrites=true&w=majority&appName=Secure"
     PHOTO_URL = ["https://telegra.ph/file/b89f8d3daed456c1c5ddf.jpg", "https://telegra.ph/file/18eb0aa99929796cf62bb.jpg"]
-    SUPPORT_CHAT = "secure_chattt"
-    UPDATE_CHAT = "secure_updates"
+    SUPPORT_CHAT = "animecatchersupport"
+    UPDATE_CHAT = "naimecatcherupdates"
     BOT_USERNAME = "animecatcher_x_robot"
-    CHARA_CHANNEL_ID = "-1002212512545"
+    CHARA_CHANNEL_ID = "-1002156837160"
     api_id = 19042248
     api_hash = "a1d443cb79941a89c493f22abf4f84cb"
 
