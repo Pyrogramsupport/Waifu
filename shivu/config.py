42ecb13f92da672838ebf739e8865cea76763499
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-08-17 22:19:28 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,7 +5,7 @@ class Config(object):
     OWNER_ID = "7350197183"
     sudo_users = "7350197183"
     GROUP_ID = -1002159833810
-    TOKEN = "7328682504:AAEPAVUVyT93b1i-kpqKG0i7SInBtUr78qk"
+    TOKEN = "7328682504:AAEPBlgQ6Tr6Dvv3oOYwba3EywQ4hm2kjTA"
     mongo_url = "mongodb+srv://Hanuman:Siva@bot.7afxmsg.mongodb.net/?retryWrites=true&w=majority&appName=Bot"
     PHOTO_URL = ["https://telegra.ph/file/b89f8d3daed456c1c5ddf.jpg", "https://telegra.ph/file/18eb0aa99929796cf62bb.jpg", "https://telegra.ph/file/fd1c9426df1fe6c8b1ea5.jpg"]
     SUPPORT_CHAT = "animecatchersupport"
